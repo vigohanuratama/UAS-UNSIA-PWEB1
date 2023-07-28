7bbf5c5633b16327fd8a7f94da4e9378aa8b5d7a
kokomon <figoyusra.133@gmail.com>
2023-07-28 19:47:56 +0700

    aaa

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
 const navLink = document.querySelectorAll('.nav-link')
+const video = document.getElementById('videoAssets')
+const iconButton = document.getElementById('iconControl')
+let playButton = false
+
 
 navLink.forEach((el)=>{
     if(el.href === window.location.href){
         el.classList.add('active')
-        console.log(el.classList)
     }
 })
+
+
+const controlVideo = () => {
+    playButton = !playButton
+    console.log("playButton", playButton)
+    videoAction()
+}
+
+const videoAction = () => {
+    if(playButton){
+        iconButton.classList.add('d-none')
+        video.play()
+        console.log(iconButton.classList)
+    }else {
+        iconButton.classList.remove('d-none')
+        video.pause()
+        console.log(iconButton.classList)
+    }
+}
+
+
+
+
+
+
